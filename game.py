25002f426825f574a86afdde4414576f070105d2
Saska1618 <atti2002@gmail.com>
2024-08-08 15:37:31 +0300

    Planets pull each other with somewhat realistic force

--- game.py
+++ game.py
@@ -3,16 +3,15 @@ from planet import Planet
 import pandas as pd
 import math
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-G = 6.6743e-11
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
 
 win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 run = True
 
 planets = []
+trail_length = 10
 
 def get_planets_from_csv():
     df = pd.read_csv('planet_data.csv')
@@ -20,13 +19,15 @@ def get_planets_from_csv():
     no_planets = df.shape[0]
 
     for i in range(no_planets):
-        planets.append(Planet(df['x'][i], df['y'][i], df['velocity'][i], df['mass'][i], (df['colorR'][i], df['colorG'][i], df['colorB'][i]), df['max_width'][i], df['max_height'][i]))
+        planets.append(Planet(df['x'][i], df['y'][i], df['velocity_x'][i], df['velocity_y'][i], df['mass'][i], (df['colorR'][i], df['colorG'][i], df['colorB'][i]), df['max_width'][i], df['max_height'][i]))
+
+
 
 
 def draw_planets():
 
     for planet in planets:
-        pygame.draw.circle(win, planet.color, (planet.x, planet.y), planet.mass)
+        pygame.draw.circle(win, planet.color, (planet.x, planet.y), planet.mass*10)
 
 def update_planets():
     for i in range(len(planets)):
@@ -39,7 +40,7 @@ def update_planets():
                 total_force_x += force_x
                 total_force_y += force_y
 
-        planets[i].move(total_force_x, total_force_y)
+        planets[i].update_position(total_force_x, total_force_y)
 
 get_planets_from_csv()
 
--- planet.py
+++ planet.py
@@ -1,29 +1,38 @@
-G = 6.6743e-11
+G = 10
 
 class Planet:
-    def __init__(self, x = 50, y = 50, velocity = 1, mass = 100, color = (0, 0, 255), max_width = 800, max_height = 600):
+    def __init__(self, x = 50, y = 50, velocity_x = 0, velocity_y = 0, mass = 100, color = (0, 0, 255), max_width = 800, max_height = 600):
         self.x = x
         self.y = y
-        self.velocity = velocity
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
         self.mass = mass
         self.color = color
 
         self.max_width = max_width
         self.max_height = max_height
 
-    def move(self, x, y):
-        self.x += (x * self.velocity * 1000000000000)
-        self.y += (y * self.velocity * 1000000000000)
+    def update_position(self, force_x, force_y, dt = 1):
 
-        if self.x < self.mass:
-            self.x = self.mass
-        if self.y < self.mass:
-            self.y = self.mass
+        acceleration_x = force_x / self.mass
+        acceleration_y = force_y / self.mass
+        
 
-        if self.x > self.max_width - self.mass:
-            self.x = self.max_width - self.mass
-        if self.y > self.max_height - self.mass:
-            self.y = self.max_height - self.mass
+        self.velocity_x += acceleration_x * dt
+        self.velocity_y += acceleration_y * dt
+
+        self.x += self.velocity_x
+        self.y += self.velocity_y 
+
+        # if self.x < self.mass:
+        #     self.x = self.mass
+        # if self.y < self.mass:
+        #     self.y = self.mass
+
+        # if self.x > self.max_width - self.mass:
+        #     self.x = self.max_width - self.mass
+        # if self.y > self.max_height - self.mass:
+        #     self.y = self.max_height - self.mass
 
     def get_gravitational_force(self, other_planet):
         distance_x = self.x - other_planet.x
